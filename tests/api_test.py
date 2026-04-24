90fee4fb46078891880186c70cf2f30899ac7f0c
dofahy <25755008+dofahy@users.noreply.github.com>
2026-04-24 08:55:07 +0100

    Rename tests/test_api.py -> tests/api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,12 +1,16 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 import os
-os.environ["DB_NAME"] = "casino-reg-ie-test"
-import pytest
-from be.db import engine, Base
-from fe.cli.main import app
 
-@pytest.fixture(scope="module")
+os.environ['DB_NAME'] = 'casino_reg_ie_test'
+import pytest
+
+from casino_reg_ie.db import Base, engine
+from casino_reg_ie.main import app
+
+
+@pytest.fixture(scope='module')
 def client():
     Base.metadata.create_all(bind=engine)
     with app.test_client() as client:
@@ -15,14 +19,13 @@ def client():
 
 
 def test_ingest_and_get(client):
-    res = client.post("/ingest", json={
-        "source": "regulation.com",
-        "content": "important information"
-    })
+    res = client.post(
+        '/ingest', json={'source': 'regulation.com', 'content': 'important information'}
+    )
     assert res.status_code == 200
-    id = res.json["id"]
-    res = client.get("/regulations")
+    id = res.json['id']
+    res = client.get('/regulations')
     assert res.status_code == 200
-    assert any(r["id"] == id for r in res.json)
+    assert any(r['id'] == id for r in res.json)
     res = client.delete(f"/regulations/{id}")
     assert res.status_code == 200
