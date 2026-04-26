d1b4b002ce117315fa374e8b56914dec2515cbc3
dofahy <25755008+dofahy@users.noreply.github.com>
2026-04-26 21:31:12 +0100

    Update tests/api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,31 +1,32 @@
-from dotenv import load_dotenv
-
-load_dotenv()
 import os
 
-os.environ['DB_NAME'] = 'regulate_ie_test'
 import pytest
+from dotenv import load_dotenv
 
-from regulate_ie.db import Base, engine
-from regulate_ie.main import app
+from regulate_ie.db import Base, get_engine
+from regulate_ie.main import app, init_app
+
+load_dotenv()
+os.environ["DB_NAME"] = "regulate_ie_test"
+init_app()
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def client():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=get_engine())
     with app.test_client() as client:
         yield client
-    Base.metadata.drop_all(bind=engine)
+    Base.metadata.drop_all(bind=get_engine())
 
 
 def test_ingest_and_get(client):
     res = client.post(
-        '/ingest', json={'source': 'regulation.com', 'content': 'important information'}
+        "/ingest", json={"source": "regulation.com", "content": "important information"}
     )
     assert res.status_code == 200
-    id = res.json['id']
-    res = client.get('/regulations')
+    id = res.json["id"]
+    res = client.get("/regulations")
     assert res.status_code == 200
-    assert any(r['id'] == id for r in res.json)
+    assert any(r["id"] == id for r in res.json)
     res = client.delete(f"/regulations/{id}")
     assert res.status_code == 200
